9b2fb95bf73a97ba9d4c26afd98a8bec3745cc05
Sam_x_developer <129406748+Samdaveloper@users.noreply.github.com>
2024-03-07 18:26:55 +0530

    Update botschk.py

--- Chizuru/modules/botschk.py
+++ Chizuru/modules/botschk.py
@@ -9,7 +9,7 @@ BOT_LIST = ["ChizuruMusicBot"]
 
 @Chizuru.on_message(filters.command("botschk") & filters.user(SUDO_USERS))
 async def bots_chk(chizuru, message):
-    msg = await message.reply_photo(photo="https://telegra.ph/file/48578068b7574bb25a529.jpg", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
+    msg = await message.reply_photo(photo="https://telegra.ph/file/f00cb69b07e7d6723ece4.png", caption="**ᴄʜᴇᴄᴋɪɴɢ ʙᴏᴛs sᴛᴀᴛs ᴀʟɪᴠᴇ ᴏʀ ᴅᴇᴀᴅ...**")
     response = "**ʙᴏᴛs sᴛᴀᴛᴜs ᴅᴇᴀᴅ ᴏʀ ᴀʟɪᴠᴇ ᴄʜᴇᴄᴋᴇʀ**\n\n"
     for bot_username in BOT_LIST:
         try:
